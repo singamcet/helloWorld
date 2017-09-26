aa19a853615b28b75b2e48fff3663ab958487407
USER <vishnugopal@factweavers.com>
2017-09-26 16:58:22 +0530

    bbbbbbbbbbbb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 //var math = require('mathjs');
 
 console.log('Loading function');
@@ -7,5 +7,17 @@ exports.handler = (event, context, callback) => {
    
     console.log("plinggggg..............wtf");
     //console.log("matha result-------------------",math.log(10000, 10))
+};*/
+
+
+
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    });
 };
 
