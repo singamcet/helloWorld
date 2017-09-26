dc1e704903960983cad3b3f907643bff148f7890
USER <vishnugopal@factweavers.com>
2017-09-26 16:57:16 +0530

    aaa

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+//var math = require('mathjs');
+
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+   
+    console.log("plinggggg..............wtf");
+    //console.log("matha result-------------------",math.log(10000, 10))
+};
+
